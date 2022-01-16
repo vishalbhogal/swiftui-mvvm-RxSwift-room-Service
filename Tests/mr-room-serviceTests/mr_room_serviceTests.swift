ebce7486936d7caf0e2bd10bd0c797efcbbb8ed0
Vishal Bhogal <bvishal3@ford.com>
2022-01-17 02:49:37 +0530

    chore: init SPM

---
+++ Tests/mr-room-serviceTests/mr_room_serviceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import mr_room_service
+
+final class mr_room_serviceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(mr_room_service().text, "Hello, World!")
+    }
+}
